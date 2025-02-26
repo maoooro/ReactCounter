de49923310c965264c753c37d570feab23155f41
MaoroHe <maorohecq1409@gmail.com>
2025-02-26 23:24:14 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <h1>My React App</h1>
-    </div>
-  )
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return <>
+    <p>Compteur : {count}</p>
+
+    <button onClick={increment}>+1</button>
+  </>
 }
 
 export default App
+
+// ep 4 : 2m20
